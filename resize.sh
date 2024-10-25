5242b4ec7ed97853dc3ce2670a849c78047f7ff4
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-10-25 14:13:32 +0200

    feat: move files to public dir

--- resize.sh
+++ resize.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-rm -rf assets/*
+rm -rf ./public/images/*
 
 for file in originals/*; do
   name=$(echo "$file" | cut -d '/' -f 2 | cut -d '.' -f 1)
+  out_dir=./public/images/"$name"
   sizes_4_3=(96 192 288 384 480 576 768 960 1152 1440 1920 2400 2880)
 
-  mkdir ./assets/"$name"
+  mkdir -p "$out_dir"
 
   echo "🖼 Processing $file"
 
   for size in "${sizes_4_3[@]}"; do
-    output=./assets/"$name"/"$size"x$((size / 4 * 3)).avif
+    output="$out_dir"/"$size"x$((size / 4 * 3)).avif
     echo creating "$output"
 
     magick "$file" -resize "$size" "$output"
